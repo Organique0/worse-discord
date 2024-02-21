776abbddcf969b7a7519244d6cc69f1e05409436
Luka Grabnar <grabnar.luka@gmail.com>
2024-02-21 06:33:24 +0100

    not hidden

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -27,7 +27,7 @@ const ServerLayout = async ({ children, params }: { children: React.ReactNode, p
     }
     return (
         <div className="h-full">
-            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+            <div className="md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
                 <ServerSidebar serverId={params.serverId} />
             </div>
             <main className="h-full md:pl-60">
